2161f8c66d305837ea930881755969d15892e782
TerraDev64 <terrawestlp@gmail.com>
2020-06-30 20:54:47 +0200

    added enemy ai

--- scenes/Monster/Monster.gd
+++ scenes/Monster/Monster.gd
@@ -4,11 +4,14 @@ onready var anim_player = $Graphics/Monster/AnimationPlayer
 onready var eyes = $Eyes
 onready var raycast = $RayCast
 
+
 enum {
 	WALK, 
 	CHASE, 
 	ATTACK
 }
+
+var death_scene = "res://Death"
 
 var state = WALK
 var target
@@ -16,47 +19,64 @@ var direction
 var fall = Vector3()
 var gravity = 0.5
 var velocity = Vector3()
+var TURN_SPEED = 2
+var attack_bool = true
+var timer = 0
+var wait_time = 5
 export var speed = 150
 
-var TURN_SPEED = -2
 
-func _ready():
-	pass
 
-	
 func _body_entered(body):
 	if body.is_in_group("player"):
-		if raycast.is_colliding():
-			var attack = raycast.is_colliding()
-			if attack.is_in_group("player"):
-				print("hit")
-		else:
-			state = CHASE
-			target = body
+		state = CHASE
+		target = body
 
 func _body_exited(body):
 	if body.is_in_group("player"):
 		state = WALK
-
+		
+func attack():
+	if raycast.is_colliding() && target.is_in_group("player"):
+		state = ATTACK
 
 func _process(delta):
-	if !is_on_floor():
+	timer -= delta
+	
+	move_and_slide(fall, Vector3.UP)
+	if not is_on_floor():
 		fall.y -= gravity
-	else:
+	if is_on_floor():
 		velocity.y = 0
 		fall.y = 0
+		
+	attack()
+		
 	match state:
 		WALK:
-			anim_player.play("walk_loop")
+			anim_player.stop()
+			#print("idle")
 		CHASE:
 			anim_player.play("walk_loop")
 			eyes.look_at(target.global_transform.origin, Vector3.UP)
-			rotate_y(deg2rad(eyes.rotation.y / TURN_SPEED))
+			rotate_y(deg2rad(eyes.rotation.y * TURN_SPEED))
 			direction = (target.transform.origin - transform.origin).normalized()
 			move_and_slide_with_snap(direction * speed * delta, Vector3.UP)
+			#print("chase")
 			
 		ATTACK:
 			anim_player.play("attack")
+			print(timer)
+			if -2.2 > timer:
+				anim_player.stop()
+			if -3 > timer:
+				get_tree().change_scene(death_scene)
+				
+				
+
+
+
+
 
 
 

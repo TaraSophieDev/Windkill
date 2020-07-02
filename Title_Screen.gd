bf889544920f52915b32647a2095e9a5c6c3c05c
TerraDev64 <terrawestlp@gmail.com>
2020-07-02 12:04:46 +0200

    added clickable icons & improved Menu

--- Title_Screen.gd
+++ Title_Screen.gd
@@ -7,3 +7,15 @@ func _start_button_pressed():
 	
 func _close_button_pressed():
 	get_tree().quit()
+
+
+func _HPS1_pressed():
+	OS.shell_open("https://hauntedps1.itch.io/")
+
+
+func _Itch_pressed():
+	OS.shell_open("https://terradev.itch.io/")
+
+
+func _Twitter_pressed():
+	OS.shell_open("https://twitter.com/TerraDev64")

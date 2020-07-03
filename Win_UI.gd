aa341913e47ab1370ed2cc6e6dd33b6fdc6aca9e
TerraDev64 <terrawestlp@gmail.com>
2020-07-03 14:16:00 +0200

    added win screen

---
+++ Win_UI.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _Retry_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+func _Menu_pressed():
+	get_tree().change_scene("res://Title_Screen.tscn")
+
+func _Close_pressed():
+	get_tree().quit()
+
